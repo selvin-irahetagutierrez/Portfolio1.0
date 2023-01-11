3d334c394b00573b80016f5daacba847430b5756
Selvinjr99 <103534433+Selvinjr99@users.noreply.github.com>
2023-01-11 12:21:21 -0500

    adding photo carasol

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#thumbnail-carousel', {
+		fixedWidth: 100,
+		gap       : 10,
+		rewind    : true,
+		pagination: false,
+  } ).mount();
+} );
